fbf49d28156f6cb0916388270faa97b3cdd71722
Nikolay Nakorkeshko <shortkedr@bk.ru>
2022-04-14 14:13:43 +0300

    update

--- Assets/ThirdParty/Krugames/LocalizationSystem/Editor/Package/PackagePathDefiner.cs
+++ Assets/ThirdParty/Krugames/LocalizationSystem/Editor/Package/PackagePathDefiner.cs
@@ -1,9 +1,21 @@
-﻿using UnityEngine;
+﻿using System.Text;
+using UnityEditor;
+using UnityEngine;
 
 namespace Krugames.LocalizationSystem.Editor.Package {
     /// <summary>
-    /// Required only to calculate path of LocalizationPackage
+    /// Calculates Localization package root folder "LocalizationSystem".
+    /// Calculation made via MonoScript and position of this script file in project
     /// </summary>
-    internal class ScriptableDummy : ScriptableObject {
+    internal class PackagePathDefiner : ScriptableObject {
+        public static string GetPackagePath() {
+            PackagePathDefiner dummy = CreateInstance<PackagePathDefiner>();
+            string fullDummyScriptPath = AssetDatabase.GetAssetPath(MonoScript.FromScriptableObject(dummy));
+            string[] pathParts = fullDummyScriptPath.Split('/');
+            StringBuilder packagePathBuilder = new StringBuilder();
+            for (int i = 0; i < pathParts.Length - 3; i++) packagePathBuilder.Append(pathParts[i]+"/");
+            Resources.UnloadUnusedAssets();
+            return packagePathBuilder.ToString();
+        }
     }
 }
--- Assets/ThirdParty/Krugames/LocalizationSystem/Editor/Package/PackageVariables.cs
+++ Assets/ThirdParty/Krugames/LocalizationSystem/Editor/Package/PackageVariables.cs
@@ -1,8 +1,4 @@
-﻿using System.Text;
-using UnityEditor;
-using UnityEngine;
-
-namespace Krugames.LocalizationSystem.Editor.Package {
+﻿namespace Krugames.LocalizationSystem.Editor.Package {
     /// <summary>
     /// Provides global Localization package variables
     /// </summary>
@@ -11,15 +7,7 @@ namespace Krugames.LocalizationSystem.Editor.Package {
         public static readonly string PackagePath;
 
         static PackageVariables() {
-            
-            ScriptableDummy dummy = ScriptableObject.CreateInstance<ScriptableDummy>();
-            string fullDummyScriptPath = AssetDatabase.GetAssetPath(MonoScript.FromScriptableObject(dummy));
-            string[] pathParts = fullDummyScriptPath.Split('/');
-            StringBuilder packagePathBuilder = new StringBuilder();
-            for (int i = 0; i < pathParts.Length - 3; i++) packagePathBuilder.Append(pathParts[i]+"/");
-            PackagePath = packagePathBuilder.ToString();
-            Resources.UnloadUnusedAssets();
-            
+            PackagePath = PackagePathDefiner.GetPackagePath();
         }
     }
 }
