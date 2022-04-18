745dd6e683ff647cf3a91207ca0957fbd61aa2d9
Nikolay Nakorkeshko <shortkedr@bk.ru>
2022-04-18 04:35:12 +0300

    add LocaleTerm generic inspector

---
+++ Assets/ThirdParty/Krugames/LocalizationSystem/Model/Editor/LocaleTermGenericInspector.cs
@@ -0,0 +1,22 @@
+﻿using Krugames.LocalizationSystem.Models;
+using UnityEditor;
+
+namespace ThirdParty.Krugames.LocalizationSystem.Model.Editor {
+    [CustomEditor(typeof(LocaleTerm<>), true)]
+    public class LocaleTermGenericInspector : UnityEditor.Editor {
+
+        private SerializedProperty _termProp;
+        private SerializedProperty _smartValueProp;
+        
+        private void OnEnable() {
+            _termProp = serializedObject.FindProperty("term");
+            _smartValueProp = serializedObject.FindProperty("smartValue");
+        }
+
+        public override void OnInspectorGUI() {
+            EditorGUILayout.PropertyField(_termProp);
+            EditorGUILayout.PropertyField(_smartValueProp);
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
