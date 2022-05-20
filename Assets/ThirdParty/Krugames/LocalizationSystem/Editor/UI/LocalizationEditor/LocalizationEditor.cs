882fb0f07c805e048491d9c5377c4d9803dd6d9d
Nikolay Nakorkeshko <shortkedr@bk.ru>
2022-05-20 14:06:35 +0300

    update LocalizationEditor remove LocalizationEditor uxml (not needed)

--- Assets/ThirdParty/Krugames/LocalizationSystem/Editor/UI/LocalizationEditor/LocalizationEditor.cs
+++ Assets/ThirdParty/Krugames/LocalizationSystem/Editor/UI/LocalizationEditor/LocalizationEditor.cs
@@ -1,10 +1,12 @@
-﻿using Krugames.LocalizationSystem.Editor.Package;
-using Krugames.LocalizationSystem.Models;
+﻿using System;
+using Krugames.LocalizationSystem.Editor.Package;
 using UnityEditor;
 using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UIElements;
 
+
+//TODO encapsulate leftPanel, rightPanel, + view to TwoSidePanelView class
 namespace Krugames.LocalizationSystem.Editor.UI.LocalizationEditor {
     
     /// <summary>
@@ -12,9 +14,37 @@ namespace Krugames.LocalizationSystem.Editor.UI.LocalizationEditor {
     /// </summary>
     public class LocalizationEditor : EditorWindow {
 
+        private enum ViewMode {
+            Plain = 0,
+            Groups = 1,
+        }
+
+        private static class EditorSaveKeys {
+            public const string ViewMode = "Krugames_LocalizationEditor_ViewMode";
+        }
+
         private static readonly Vector2 MinSize = new Vector2(800, 500);
         private static readonly Vector2 DefaultSize = new Vector2(800, 500);
 
+        
+        [SerializeField] private ViewMode viewMode;
+        
+        
+        private VisualElement _root;
+
+        private Toolbar _toolbar; // TODO ElementToolbar, left, right, middle
+        private Button _leftPanelToggleButton; //TODO ToggleButton, place to left
+        private Button _rightPanelToggleButton; //TODO ToggleButton, place to right
+        private EnumField _viewModeSelector; //TODO ButtonEnum, place to center;
+
+        private VisualElement _contentRoot;
+
+        private VisualElement _contentView;
+        private VisualElement _leftPanel;
+        private VisualElement _rightPanel;
+        
+
+        
         [MenuItem("Window/Krugames/Localization")]
         public static void Open() {
             LocalizationEditor editorWindow = EditorWindow.GetWindow<LocalizationEditor>("Localization", true);
@@ -23,32 +53,179 @@ namespace Krugames.LocalizationSystem.Editor.UI.LocalizationEditor {
             editorWindow.Show();
         }
 
-        public LocalizationEditor() {
+        public void CreateGUI() {
+            SerializedObject serializedObject = new SerializedObject(this);
+            
+            ImportStyleSheet();
+            
+            _root = rootVisualElement;
+
+            _toolbar = new Toolbar() {
+                style = {
+                    minHeight = 24,
+                    maxHeight = 24,
+                    flexGrow = 1,
+                }
+            };
+
+            _leftPanelToggleButton = new ToolbarButton(ToggleLeftPanel) {
+                text = "<"
+            };
+
+            _rightPanelToggleButton = new ToolbarButton(ToggleRightPanel) {
+                text = ">"
+            };
+
+            _viewModeSelector = new EnumField("View mode", ViewMode.Plain);
+            _viewModeSelector.BindProperty(serializedObject.FindProperty("viewMode"));
+            
+            _toolbar.Add(_leftPanelToggleButton);
+            _toolbar.Add(_viewModeSelector);
+            _toolbar.Add(_rightPanelToggleButton);
+
+            _contentRoot = new VisualElement() {
+                style = {
+                    flexGrow = 1,
+                    flexDirection = new StyleEnum<FlexDirection>(FlexDirection.Row),
+                }
+            };
+            
+            _contentView = new VisualElement() {
+                style= {
+                    flexGrow = 1,
+                    width = new StyleLength(StyleKeyword.Auto),
+                    alignItems = new StyleEnum<Align>(Align.Stretch),
+                }
+            };
+            
+            _leftPanel = new VisualElement() {
+                style= {
+                    flexGrow = 0,
+                    minWidth = 300,
+                    maxWidth = 300,
+                    width = new StyleLength(StyleKeyword.Auto),
+                    borderRightWidth = 1,
+                    borderRightColor = new Color(0, 0, 0, 0.5f), //TODO replace with variable in uss;
+                    alignItems = new StyleEnum<Align>(Align.Stretch),
+                }
+            };
+            
+            _rightPanel = new VisualElement() {
+                style= {
+                    flexGrow = 0,
+                    minWidth = 300,
+                    maxWidth = 300,
+                    width = new StyleLength(StyleKeyword.Auto),
+                    borderLeftWidth = 1,
+                    borderLeftColor = new Color(0, 0, 0, 0.5f), //TODO replace with variable in uss;
+                    alignItems = new StyleEnum<Align>(Align.Stretch),
+                }
+            };
+            
+            _contentRoot.Add(_contentView);
+            ToggleLeftPanel();
+
+            _root.Add(_toolbar);
+            _root.Add(_contentRoot);
+
+            var localesBox = new TittledContentBox("Locales") {
+                style = {
+                    height = new StyleLength(StyleKeyword.Auto),
+                    alignSelf = new StyleEnum<Align>(Align.Stretch),
+                    flexGrow = 1,
+                }
+            };
+
+            var editBox = new TittledContentBox("English (158 terms, 97 notes, 13 empty)") {
+                style = {
+                    height = new StyleLength(StyleKeyword.Auto),
+                    alignSelf = new StyleEnum<Align>(Align.Stretch),
+                    flexGrow = 1,
+                }
+            };
+
+            var functionsBox = new TittledContentBox("Functions") {
+                style = {
+                    height = new StyleLength(StyleKeyword.Auto),
+                    alignSelf = new StyleEnum<Align>(Align.Stretch),
+                    flexGrow = 1,
+                }
+            };
+
+            for (int i = 0; i < 3; i++) localesBox.Content.Add(new Button(){text="Locale "+i.ToString()});
+            for (int i = 0; i < 17; i++) editBox.Content.Add(new Button(){text="Term "+i.ToString()});
+            for (int i = 0; i < 4; i++) functionsBox.Content.Add(new Button(){text="Function "+i.ToString()});
+
+            _leftPanel.Add(localesBox);
+            _contentView.Add(editBox);
+            _rightPanel.Add(functionsBox);
+        }
+
+        private void Awake() {
+            LoadEditorValues();
         }
         
-        public void CreateGUI() {
-            VisualElement root = rootVisualElement;
-            
-            string uxmlPath = PackageVariables.PackagePath + "Editor/UI/LocalizationEditor/LocalizationEditor.uxml";
+        private void OnDestroy() {
+            SaveEditorValues();
+        }
+
+        private void ImportStyleSheet() {
             string ussPath = PackageVariables.PackagePath + "Editor/UI/LocalizationEditor/LocalizationEditor.uss";
-
-            VisualTreeAsset visualTreeAsset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(uxmlPath);
             StyleSheet styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>(ussPath);
-
-            if (visualTreeAsset != null) visualTreeAsset.CloneTree(rootVisualElement);
-            else Debug.LogError($"{nameof(LocalizationEditor)} uxml schema asset not found!");
-
             if (styleSheet != null) rootVisualElement.styleSheets.Add(styleSheet);
             else Debug.LogError($"{nameof(LocalizationEditor)} uss style sheet asset not found!");
-            
-            AssignVisualTreeComponents();
         }
 
-        private void AssignVisualTreeComponents() {
-            /*LocaleLibrary library = LocaleLibrary.Instance;
-            SerializedProperty property = new SerializedObject(library).FindProperty("baseLocale");
-            rootVisualElement.Q<PropertyField>("BaseLocaleField").BindProperty(property);*/
+        private void SetLeftPanelState(bool state) {
+            if (state) {
+                _contentRoot.Add(_leftPanel);
+                _leftPanel.PlaceBehind(_contentView);
+            } else {
+                _leftPanel.RemoveFromHierarchy();
+            }
+        }
+
+        private void SetRightPanelState(bool state) {
+            if (state) {
+                _contentRoot.Add(_rightPanel);
+                _rightPanel.PlaceInFront(_contentView);
+            } else {
+                _rightPanel.RemoveFromHierarchy();
+            }
         }
         
+        private void ToggleLeftPanel() {
+            SetLeftPanelState(!GetLeftPanelState());
+        }
+
+        private void ToggleRightPanel() {
+            SetRightPanelState(!GetRightPanelState());
+        }
+
+        private bool GetLeftPanelState() {
+            return _contentRoot.Contains(_leftPanel);
+        }
+
+        private bool GetRightPanelState() {
+            return _contentRoot.Contains(_rightPanel);
+        }
+
+        private void SetDefaultEditorValues() {
+            viewMode = ViewMode.Plain;
+        }
+        
+        private void SaveEditorValues() {
+            EditorPrefs.SetString(EditorSaveKeys.ViewMode, viewMode.ToString());
+        }
+
+        private void LoadEditorValues() {
+            SetDefaultEditorValues();
+            
+            if (EditorPrefs.HasKey(EditorSaveKeys.ViewMode)) {
+                string value = EditorPrefs.GetString(EditorSaveKeys.ViewMode);
+                Enum.TryParse(value, out viewMode);
+            }
+        }
+
     }
 }
---
+++ Assets/ThirdParty/Krugames/LocalizationSystem/Editor/UI/LocalizationEditor/TittledContentBox.cs
@@ -0,0 +1,46 @@
+﻿using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Krugames.LocalizationSystem.Editor.UI.LocalizationEditor {
+    public class TittledContentBox : Box {
+
+        private Toolbar _toolbar;
+        private Label _tittle;
+        private VisualElement _content;
+        
+        public Label Tittle => _tittle;
+        public VisualElement Content => _content;
+
+        public TittledContentBox(string tittle) {
+
+            _toolbar = new Toolbar() {
+                style = {
+                    flexGrow = 1f,
+                    minHeight = 21,
+                    maxHeight = 21,
+                }
+            };
+            
+            _tittle = new Label(tittle) {
+                style = {
+                    flexGrow = 1f,
+                    unityTextAlign = new StyleEnum<TextAnchor>(TextAnchor.MiddleCenter),
+                    unityFontStyleAndWeight = new StyleEnum<FontStyle>(FontStyle.Bold),
+                }
+            };
+
+            _content = new VisualElement() {
+                style = {
+                    flexGrow = 1f,
+                    backgroundColor = new Color(0f, 0f, 0f, 0f),
+                }
+            };
+            
+            _toolbar.Add(_tittle);
+            
+            Add(_toolbar);
+            Add(_content);
+        }
+    }
+}
